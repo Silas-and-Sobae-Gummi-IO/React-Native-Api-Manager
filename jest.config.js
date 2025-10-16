6936d6c4f0e05fb49f8b29cec1b2b54f1d10bf32
Alan Chen <alan.chen.la.478@gmail.com>
2025-10-16 14:24:23 -0700

    Setup - Create the project structure and initialize your testing framework

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // Tells Jest to look for tests only in the 'src' directory.
+  roots: ['<rootDir>/src'],
+
+  // The test environment that will be used for testing.
+  testEnvironment: 'node',
+};
